2d8364b6007d3e87d8836f2245f6f48e5b74c429
JosueDev09 <josuecode27@gmail.com>
2024-12-10 23:03:18 -0600

    Quiero centrar mi texto junto con el video

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,ts}', // Agrega todas las extensiones y directorios relevantes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
