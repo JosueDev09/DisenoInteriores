ba07089c672011989a67cfc1c9ef68d0d90f4191
JosueFG <josueflores13199@gmail.com>
2024-12-13 17:21:34 -0600

    Se implemento el navbar y se arreglo para que sea responsive, empezar con las demas pantallas

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     './src/**/*.{astro,html,js,ts}', // Agrega todas las extensiones y directorios relevantes
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gradient-start': '#01EB6A',
+        'gradient-end': '#0085EC',
+      },
+    },
   },
   plugins: [],
 }
