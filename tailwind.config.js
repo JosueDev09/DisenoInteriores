75b86bae2066ee61db1cda9280cb7f361e3b5870
JosueDev09 <josuecode27@gmail.com>
2024-12-21 19:42:23 -0600

    Intento de arreglar el burguer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{astro,html,js,ts}', // Agrega todas las extensiones y directorios relevantes
-  ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
       colors: {
-        'gradient-start': '#01EB6A',
-        'gradient-end': '#0085EC',
-        'body': ['"Open Sans"'],
+        'bg-gray-cumtual': '#191A1E',
+        'green-cumtual': '#04E95F',
+        'blue-cumtual': '#007BEA'
       },
+      extend: {
+        animation: {
+          'infinite-scroll': 'infinite-scroll 13s linear infinite',
+          },
+          keyframes: {
+          'infinite-scroll': {
+            from: { transform: 'translateX(0)' },
+            to: { transform: 'translateX(-100%)' },
+          }
+        }
     },
   },
-  plugins: [],
+
+    plugins: [typography],
 }
 
